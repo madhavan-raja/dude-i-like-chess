f4975769aa5eaaad95e6cfdbbd39e1a5a94cf18e
madhavan-raja <madhavanraja99@gmail.com>
2021-11-06 22:26:53 +0530

    Added files

---
+++ main.py
@@ -0,0 +1,19 @@
+import chess
+import random
+
+board = chess.Board()
+is_player_turn = True
+
+while not board.is_game_over():
+  print(board)
+  print()
+
+  if is_player_turn:
+    current_move = input("Enter current move: ")
+    current_move = board.parse_san(current_move)
+  else:
+    current_move = random.choice(list(board.legal_moves))
+    print(f"Computer plays {current_move}")
+
+  board.push(current_move)
+  is_player_turn = not is_player_turn
